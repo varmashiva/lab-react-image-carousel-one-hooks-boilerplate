1ad746846f652dbd3303444668b3d73cd424435d
Shiva Varma <shivavarma@Shivas-MacBook-Air.local>
2024-02-28 16:51:50 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import React, { Component } from "react";
+import Carousel from "./Components/Carousel";
 import "./App.css";
-import Carousel from "./components/Carousel";
-import React from "react";
-function App() {
-  return (
-    <div className="App">
-      <Carousel />
-    </div>
-  );
-}
 
-export default App;
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Carousel />
+      </div>
+    );
+  }
+}
